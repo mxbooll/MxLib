d7aec8d61b6a4a03553c74ab8ab7f24d751c0cf7
mxbooll <mxbooll@gmail.com>
2024-01-14 16:17:03 +0500

    Added react application and settings for it.

---
+++ frontend/web-app/src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+
+import test from './test.module.css'
+import Home from './routes/Home'
+
+export default function App() {
+  console.log(test)
+  return (
+      <div className="App">
+        <h1 id={test.text}>Welcome to React Router!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+  );
+}
---
+++ frontend/web-app/src/routes/Home.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import styles from './Home.module.css'
+
+type CardProps = {
+    title?: string,
+    paragraph?: string
+}
+
+const home = ({ title, paragraph }: CardProps) =>{
+    const [counter, setCounter] = useState(0)
+
+    return (
+        <aside>
+            <div id={styles.homeButton} onClick={()=>{console.log("test");setCounter(counter + 1)}}>
+                Hello World = {counter}
+            </div>
+
+        </aside>
+    )
+}
+
+export default home
+
